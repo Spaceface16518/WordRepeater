aa1d9e3fa78781297c9f50b122a81f5c21cabb06
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-05-10 21:50:13 -0500

    Add a few tests
    Signed-off-by: Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>

--- src/com/repeater/GeneratorTest.java
+++ src/com/repeater/GeneratorTest.java
@@ -1,74 +1,17 @@
 package com.repeater;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GeneratorTest {
 
     private Generator repeater;
 
-    @BeforeEach
-    void init() {
+    @Test
+    @Tag("Default")
+    void testDefault() {
         repeater = new Generator("This is a test", ' ');
+        repeater.forEach(System.out::println);
     }
 
-    @Test
-    @DisplayName("next method")
-    void testNext() {
-        assertEquals(repeater.getWord(repeater.getIndex() + 1), repeater.next());
-    }
-
-    @Test
-    @DisplayName("Auto-increment method")
-    void testIncrementIndex() {
-        for (int i = 0; i < repeater.size(); i++) {
-            assertEquals(i, repeater.getIndex());
-            repeater.incrementIndex();
-        }
-    }
-
-    @Test
-    @DisplayName("Method to increment by an amount")
-    void testIncrementIndexByAmount() {
-        int rand = new Random().nextInt(repeater.size());
-        repeater.incrementIndex(rand);
-        assertEquals(rand, repeater.getIndex());
-    }
-
-    @Test
-    void getIndex() {
-        assertEquals(0, repeater.getIndex());
-    }
-
-    @Test
-    void setIndex() {
-        repeater.setIndex(1);
-        assertEquals(1, repeater.getIndex());
-    }
-
-    @Test
-    @Disabled
-    void getWord() {
-    }
-
-    @Test
-    @Disabled
-    void size() {
-    }
-
-    @Test
-    @Disabled
-    void getCurrent() {
-    }
-
-    @Test
-    @Disabled
-    void forEach() {
-    }
 }

d12b3d317c45ff22340e674d48a6cbdd50c8729c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-05-11 16:13:03 -0500

    Optimize Generator Test

--- src/com/repeater/GeneratorTest.java
+++ src/com/repeater/GeneratorTest.java
@@ -4,14 +4,10 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 class GeneratorTest {
-
-    private Generator repeater;
-
     @Test
     @Tag("Default")
     void testDefault() {
-        repeater = new Generator("This is a test", ' ');
+        Generator repeater = new Generator("This is a test", ' ');
         repeater.forEach(System.out::println);
     }
-
 }

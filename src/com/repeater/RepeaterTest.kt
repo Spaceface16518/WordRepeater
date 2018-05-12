a17d0d752d9b02b67f2226204c32b65cf8afe9d4
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-05-11 19:40:07 -0500

    Change RepeaterTest language to Kotlin
    Java was unnecessary for such a simple test class.
    
    Signed-off-by: Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>

---
+++ src/com/repeater/RepeaterTest.kt
@@ -0,0 +1,42 @@
+package com.repeater
+
+import org.junit.jupiter.api.*
+
+internal class RepeaterTest {
+
+    private var repeater: Repeater? = null
+
+    @BeforeAll
+    fun init() {
+        repeater = Repeater()
+    }
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @Test
+    @Tag("Class")
+    @DisplayName("Default, full class test")
+    fun testClass() {
+        // Constructor not yet defined
+    }
+
+    @Test
+    @Tag("flushStreams")
+    @DisplayName("test flushStreams")
+    fun testFlushStreams() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+        repeater!!.flushStreams()
+    }
+
+    @AfterAll
+    fun close() {
+
+    }
+}
